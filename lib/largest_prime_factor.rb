5cd31910886866c13b8f596e09a1b6f401dab7ea
boom100100 <bsd231@nyu.edu>
2020-09-17 14:38:29 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,11 @@
 # Enter your procedural solution here!
+require "prime"
+def largest_prime_factor(input)
+  factor = 2
+  while factor < input
+    result = input / factor
+    return result if input % factor == 0 && Prime.prime?(result)
+    factor += 1
+    # return result if
+  end
+end
