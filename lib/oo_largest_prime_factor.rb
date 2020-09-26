20cbc4fda794a61176e86a74a8f24e48abd2594c
trishogen <patriciaogen@gmail.com>
2020-09-25 22:04:53 -0400

    Done.

--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,35 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+
+  def initialize(n)
+    @n = n
+  end
+
+  def number
+    while @n.even?
+      # divide by 2 until you can't anymore
+      @n = @n/2
+    end
+
+    if @n <= 3
+      return @n
+    end
+
+    i = 3
+    highest_prime = @n
+    sqrt_n  = Math.sqrt(@n)
+
+    while i <= sqrt_n
+      # number cannot be divisble by a number bigger than it's square root
+      if @n % i == 0
+        @n = @n/i
+        highest_prime = i
+      end
+
+      # can skip even numbers at this point since we already divided out all the 2s
+      i = i + 2
+    end
+
+    return highest_prime
+  end
+end
