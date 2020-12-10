d77f6db528c05a11582ca7e87042e45587afe0a4
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-12-10 19:32:26 +0100

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,27 @@
 # Enter your procedural solution here!
+def prime?(n)
+    if n <= 1
+      return false
+    else 
+        if (2..Math.sqrt(n).floor).any? { |i| n % i === 0}
+            return false
+        else
+            return true
+        end 
+    end
+end
+
+def largest_prime_factor(num)
+    largest = 1
+    counter = 1
+
+    until counter === Math.sqrt(num).floor
+        if prime?(counter) && num % counter === 0
+            largest = counter
+        end
+        counter += 1
+    end
+
+    largest
+end
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,34 @@
 # Enter your object-oriented solution here!
+class LargestPrimeFactor
+    attr_reader :num
+
+    def initialize(num)
+        @num = num
+    end
+
+    def prime?(n)
+        if n <= 1
+          return false
+        else 
+            if (2..Math.sqrt(n).floor).any? { |i| n % i === 0}
+                return false
+            else
+                return true
+            end 
+        end
+    end
+    
+    def number
+        largest = 1
+        counter = 1
+    
+        until counter === Math.sqrt(@num).floor
+            if prime?(counter) && @num % counter === 0
+                largest = counter
+            end
+            counter += 1
+        end
+    
+        largest
+    end
+end
