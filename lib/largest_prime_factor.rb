4c283c8014db4b0af0b128092833bd1a6cb42285
Alexander M. Clamor <alexander.clamor@gmail.com>
2021-05-04 17:50:09 -0700

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,29 @@
-# Enter your procedural solution here!
+def largest_prime_factor(input)
+
+    # find all factors
+    a = 1
+    factors = []
+    
+    while a <= input
+        if (input % a) == 0 
+            factors.push(a)
+        end
+        a += 1
+    end
+    
+    # find factors that are prime numbers
+    prime_factors = factors.select do |factor|
+        if (factor == 1 || factor == input)
+            false
+        else
+            (2..factor - 1).none? do |f|
+                factor % f == 0
+            end
+        end
+    end
+
+    # select largest prime factor
+    prime_factors.max
+end
+
+puts largest_prime_factor(600851475143)
